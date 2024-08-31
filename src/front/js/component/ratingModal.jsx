150bdb9b73e1292ac2398401996e68f05e65f4cc
Hugo Ortecho <161871340+UgoVoz@users.noreply.github.com>
2024-08-31 17:02:10 +0000

    avance Modal

--- src/front/js/component/ratingModal.jsx
+++ src/front/js/component/ratingModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 export const RatingModal = () => {
+
     return (
         <div className="modal" id="ratingModal">
             <div class="modal-dialog">
@@ -10,11 +11,32 @@ export const RatingModal = () => {
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <p>Modal body text goes here.</p>
+                
+            <div class="mb-3">
+                <label for="formFile" class="form-label">Default file input example</label>
+                <input class="form-control" type="file" id="formFile" />
+                </div>
+                <div class="mb-3">
+                <label for="formFileMultiple" class="form-label">Multiple files input example</label>
+                <input class="form-control" type="file" id="formFileMultiple" multiple />
+                </div>
+                <div class="mb-3">
+                <label for="formFileDisabled" class="form-label">Disabled file input example</label>
+                <input class="form-control" type="file" id="formFileDisabled" disabled />
+                </div>
+                <div class="mb-3">
+                <label for="formFileSm" class="form-label">Small file input example</label>
+                <input class="form-control form-control-sm" id="formFileSm" type="file" />
+                </div>
+                <div>
+                <label for="formFileLg" class="form-label">Large file input example</label>
+                <input class="form-control form-control-lg" id="formFileLg" type="file" />
+                </div>
+
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-primary">Enviar Valoración</button>
             </div>
             </div>
         </div>
