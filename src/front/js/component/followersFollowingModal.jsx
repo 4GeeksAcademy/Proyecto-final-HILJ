c3bc7da30608078787296cff0026c1c9b3c38ad5
Hugo Ortecho <161871340+UgoVoz@users.noreply.github.com>
2024-09-02 14:08:50 +0000

    Conectar Modal al back

--- src/front/js/component/followersFollowingModal.jsx
+++ src/front/js/component/followersFollowingModal.jsx
@@ -1,24 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MiniProfileCard from "../component/miniProfileCard.jsx";
 
 
-export const FollowersFollowingModal = () => {
+export const FollowersFollowingModal = ({ userId }) => {
 
     const [activeTab, setActiveTab] = useState('followers');
+    const [followers, setFollowers] = useState([]);
+    const [following, setFollowing] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-    const followers = [
-        { name: 'Pepe Pérez', username: 'pepepe', imageUrl: 'https://cdn.pixabay.com/photo/2024/08/22/22/03/backpacker-8990243_1280.png'},
-        { name: 'Lola López', username: 'lolalo', imageUrl: 'https://media.istockphoto.com/id/1192648836/es/foto/joven-mujer-feliz-en-una-cabina-de-avi%C3%B3n.jpg?s=1024x1024&w=is&k=20&c=1ngB5TbUh_LebZrHA9J9CaHX_nesH47G_QX5zm18008='},
-        { name: 'Pablo Suarez', username: 'psuarez', imageUrl: 'https://cdn.pixabay.com/photo/2018/05/04/12/21/man-3373868_1280.jpg'},
-        { name: 'Luis Llanos', username: 'lllanos', imageUrl: 'https://cdn.pixabay.com/photo/2016/08/20/08/46/hiker-1607078_1280.jpg'},
-    ];
+    useEffect(() => {
+        if (show.bs.modal) {
+            setLoading(true);
+            if (activeTab === 'followers') {
+                
+            }
+        }
+    })
 
-    const following = [
-        { name: 'Jack Wilson', username: 'jwilson', imageUrl: 'https://cdn.pixabay.com/photo/2021/04/28/07/45/man-6213257_1280.jpg'},
-        { name: 'Lucas Luna', username: 'luluna', imageUrl: 'https://media.istockphoto.com/id/1042600048/es/foto/hombre-de-senderismo-en-los-alpes-suizos-en-appenzell.jpg?s=1024x1024&w=is&k=20&c=IIEmlUOV0EwzOAlRLhQyzaClP7Xx8Sp-BrcnKntDBQ8='},
-        { name: 'lylly Carter', username: 'llycart', imageUrl: 'https://cdn.pixabay.com/photo/2016/11/16/03/37/globe-trotter-1828079_1280.jpg'},
-        { name: 'Lucia Prendez', username: 'lprendez', imageUrl: 'https://cdn.pixabay.com/photo/2017/08/29/12/07/adult-2693054_1280.jpg'},
-    ];
+    //const followers = [
+    //    { name: 'Pepe Pérez', username: 'pepepe', imageUrl: 'https://cdn.pixabay.com/photo/2024/08/22/22/03/backpacker-8990243_1280.png'},
+    //    { name: 'Lola López', username: 'lolalo', imageUrl: 'https://media.istockphoto.com/id/1192648836/es/foto/joven-mujer-feliz-en-una-cabina-de-avi%C3%B3n.jpg?s=1024x1024&w=is&k=20&c=1ngB5TbUh_LebZrHA9J9CaHX_nesH47G_QX5zm18008='},
+    //    { name: 'Pablo Suarez', username: 'psuarez', imageUrl: 'https://cdn.pixabay.com/photo/2018/05/04/12/21/man-3373868_1280.jpg'},
+    //    { name: 'Luis Llanos', username: 'lllanos', imageUrl: 'https://cdn.pixabay.com/photo/2016/08/20/08/46/hiker-1607078_1280.jpg'},
+    //];
+
+    //const following = [
+    //    { name: 'Jack Wilson', username: 'jwilson', imageUrl: 'https://cdn.pixabay.com/photo/2021/04/28/07/45/man-6213257_1280.jpg'},
+    //    { name: 'Lucas Luna', username: 'luluna', imageUrl: 'https://media.istockphoto.com/id/1042600048/es/foto/hombre-de-senderismo-en-los-alpes-suizos-en-appenzell.jpg?s=1024x1024&w=is&k=20&c=IIEmlUOV0EwzOAlRLhQyzaClP7Xx8Sp-BrcnKntDBQ8='},
+    //    { name: 'lylly Carter', username: 'llycart', imageUrl: 'https://cdn.pixabay.com/photo/2016/11/16/03/37/globe-trotter-1828079_1280.jpg'},
+    //    { name: 'Lucia Prendez', username: 'lprendez', imageUrl: 'https://cdn.pixabay.com/photo/2017/08/29/12/07/adult-2693054_1280.jpg'},
+    //];
     
     return (
         <div className="modal" id="followersFollowingModal" tabindex="-1">
